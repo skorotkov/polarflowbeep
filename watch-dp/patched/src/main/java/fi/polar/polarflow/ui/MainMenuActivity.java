a41949c1abdc2d35fb71cff88f4ca4ed2360935c
Sergey Korotkov <sergey.korotkov@prowidelabs.com>
2018-08-27 23:04:56 +0700

    override version number in main menu

--- watch-dp/patched/src/main/java/fi/polar/polarflow/ui/MainMenuActivity.java
+++ watch-dp/patched/src/main/java/fi/polar/polarflow/ui/MainMenuActivity.java
@@ -3,7 +3,9 @@ package fi.polar.polarflow.ui;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.bg;
+import android.widget.TextView;
 
+import fi.polar.polarflowbeep.R;
 import lanchon.dexpatcher.annotation.DexAppend;
 import lanchon.dexpatcher.annotation.DexEdit;
 import lanchon.dexpatcher.annotation.DexIgnore;
@@ -16,6 +18,14 @@ public class MainMenuActivity extends Activity implements bg {
         // After executing the original onCreate method, replace the text and handler
         // of the device ID 'Copy' button to make it a 'Share' button instead.
         SoundManager.init(this);
+    }
+
+    @DexIgnore
+    private TextView f;
+
+    @DexAppend
+    private void c() {
+        this.f.setText(this.getString(R.string.main_menu_version_1_row, new Object[]{"2.3.0-beep"}));
     }
 
     @Override @DexIgnore public void a(int i) { throw null; }
