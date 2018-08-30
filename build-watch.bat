3f0d616f23ad00c86c0b182febae3fa000fd2f0e
Sergey Korotkov <sergey.korotkov@prowidelabs.com>
2018-08-30 12:46:06 +0700

    add patch for 3.7.6 phone apk

--- build-watch.bat
+++ build-watch.bat
@@ -1,7 +1,2 @@
-rem java -jar .\tools\apktool\apktool_2.3.3.jar b -d .\watch -o PolarFlowBeep_2.3.0-unaligned.apk
-rem jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore keystore\polarflowbeep.jks -storepass polarflowbeep PolarFlowBeep_2.3.0-unaligned.apk key0
-rem .\tools\testsign\zipalign -f -v 4 PolarFlowBeep_2.3.0-unaligned.apk PolarFlowBeep_2.3.0.apk
-rem del PolarFlowBeep_2.3.0-unaligned.apk
 cd watch-dp
 gradlew.bat assembleRelease
-cd ..
