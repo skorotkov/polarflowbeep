4470c9a5a6036a7f3e5f6fadccaaf97590f0023e
Sergey Korotkov <sergey.korotkov@prowidelabs.com>
2018-09-12 15:07:38 +0700

    substitute GpsLocationProvider with the Fused one

---
+++ watch-dp/patched/src/main/java/fi/polar/polarflow/c_patch/r.java
@@ -0,0 +1,33 @@
+package fi.polar.polarflow.c_patch;
+
+import android.content.Context;
+
+import java.util.Map;
+
+import fi.polar.polarflow.sensor.fusedgps.FusedGpsLocationProvider;
+import lanchon.dexpatcher.annotation.DexEdit;
+import lanchon.dexpatcher.annotation.DexIgnore;
+import lanchon.dexpatcher.annotation.DexPrepend;
+
+import fi.polar.polarflow.c.a;
+
+@DexEdit(target = "fi.polar.polarflow.c.r", contentOnly = true)
+public class r { // SessionSensorsThread
+    @DexIgnore private final Map e = null;
+    @DexIgnore private boolean c(int var1) { throw null; }
+    @DexIgnore private final Context b = null;
+
+    @DexIgnore
+    private r() { throw null; }
+
+    @DexPrepend
+    private void b() { // createSensors
+        if (this.c(4) && this.e.get(4) == null) {
+            Object gpsProvider = new FusedGpsLocationProvider(this.b);
+            if (gpsProvider != null) {
+                this.e.put(4, gpsProvider);
+                ((a)gpsProvider).b(); // ((a_Sensor)gpsProvider).b_start();
+            }
+        }
+    }
+}
